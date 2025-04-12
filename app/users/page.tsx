c40ba02107b501ca69cc13c167e535e60d835f6e
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-04-12 08:27:41 -0400

    fetch data using server components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,29 @@
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+// define an interface for the User object
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  // give the [data url] to the {fetch} function. await it and put it in a [res] variable
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  // use the [res] {json} method to convert the response to JSON and put it in a [users] variable.
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
