254ddddefb0277206abb2fbd851ab8742cb1e270
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-04-12 08:35:28 -0400

    add caching option to fetch call

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,12 @@ interface User {
 
 const UsersPage = async () => {
   // give the [data url] to the {fetch} function. await it and put it in a [res] variable
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  // cache: "no store" for when you have fast changing data
+  // to keep data fresh for a certain amount of time you set next: { revalidate: 10 } which means it get infoomation every 10 seconds
+  // this caching behavior is only acessible in the {fetch} function
 
   // use the [res] {json} method to convert the response to JSON and put it in a [users] variable.
   const users: User[] = await res.json();
