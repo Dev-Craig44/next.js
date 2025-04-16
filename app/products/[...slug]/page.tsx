8d1893d2d0c00477f1d1f5e5cc36d814a92671cf
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-04-16 12:24:33 -0400

    ✅ create slug routing for Products

--- app/products/[...slug]/page.tsx
+++ app/products/[...slug]/page.tsx
@@ -1,5 +1,11 @@
-const ProductPage = () => {
-  return <div>ProductPage</div>;
+// create interface with [slug] as string[] and use it in the component
+interface Props {
+  params: { slug: string[] };
+}
+// destructure [params] from [Props]
+//destructure [slug] from [params]
+const ProductPage = ({ params: { slug } }: Props) => {
+  return <div>ProductPage {slug}</div>;
 };
 
 export default ProductPage;
