59a3fe0be64e140ba649ab5c71259739b4152750
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-04-15 23:24:15 -0400

    🔍 fixed setup for postcss

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    "@tailwindcss/postcss", // ✅ As a string, NOT require()
+    "autoprefixer",
+  ],
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
+
+export default config;
