a53089814fb4d392608d0cce4375df805d55b982
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-04-16 01:18:28 -0400

    ✅ change ul to a table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -20,14 +20,23 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <h2>{user.name}</h2>
-          </li>
-        ))}
-      </ul>
+      {/* change ul to a table */}
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
