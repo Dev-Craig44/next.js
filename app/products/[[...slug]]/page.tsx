8ace99ca8a794c906c36a9e5991d1d04eda33a36
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-04-16 12:51:13 -0400

    ✅ add searchParams to slug router

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,11 +1,23 @@
 // create interface with [slug] as string[] and use it in the component
 interface Props {
   params: { slug: string[] };
+
+  // to access query string parameters we use a different method called useSearchParams
+  searchParams: { sortOrder: string };
 }
 // destructure [params] from [Props]
 //destructure [slug] from [params]
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>ProductPage {slug}</div>;
+// destructure [searchParams] from [Props]
+// destructure [sortOrder] from [searchParams]
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <div>
+      ProductPage {slug} {sortOrder}
+    </div>
+  );
 };
 
 export default ProductPage;
