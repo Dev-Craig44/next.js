70836d510a4eba29a813b915cf0f376212cf7cf4
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-05-06 09:17:50 -0400

    🧠 create endpoint to update an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -12,3 +12,22 @@ export function GET(
 
   return NextResponse.json({ id: 1, name: "Craig" });
 }
+
+// you can use put or patch
+// you should use put for replacing an object and patch for updating one or more properties
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // validate the request body
+  // if invalid, return 400 bad request
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // fetch the user with the given id from the db
+  // if not found, return 404 (user not found)
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // else update the user and return the updated user
+  return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+}
--- building-api.md
+++ building-api.md
@@ -11,6 +11,14 @@ Getting a Collection of Objects
 - in a gived folder or URL segment we can either have a page file or a route file, but not both
 - if you want to show something to the user, you need to use a page file
 
+Getting a Single Object
 _create endpoint for getting a single object_
 
+Creating an Object
+
 - to create a user, we send a request to the user's endpoint include a user object in the body of the request
+
+Updating an Object
+
+- update a user, we should send a request to the endpoint that represents an individual user, like users/1
+- so we send the request and include the updated user object in the body of the request
