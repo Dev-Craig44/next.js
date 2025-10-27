e310b774b1d8bd8fcb79b7e9d09046fcd5bc597f
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-10-26 21:35:45 -0400

    🧼

--- recap/recap/app/users/schema.ts
+++ recap/recap/app/users/schema.ts
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
-// This is a simple schema for validating user data
-// In a real world app, you would use a more complex schema
-// with more fields and validation rules
 const schema = z.object({
   name: z.string().min(3),
-  // 3.) make email required and validate it as an email
   email: z.string().email(),
 });
 
